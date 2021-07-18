eceda6c3e6d7970738e014ba70e9e342e1a2a413
atharv1215 <74772093+atharv1215@users.noreply.github.com>
2021-07-18 19:53:33 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+// add code for displaying rest of the names and the hints.
+    
+ fill("white")
+ textSize(15)
+ text("A T A D E S B A", 690,170)
+ fill("lightblue")
+ text("Hint: A systematic collection of data !!", 690,190)
+
+ fill("white")
+ textSize(15)
+ text("C U T N I F O N", 300,250)
+ fill("lightblue")
+ text("Hint: Used to take in data, process it and return a result !!", 300,270)
+
+}
